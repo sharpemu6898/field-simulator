a75504b926b9bcdec906e6d024f0ef11c60a2ee8
sharpemu6898 <133412975+sharpemu6898@users.noreply.github.com>
2026-09-25 08:04:09 -0500

    estructura

---
+++ ui.js
@@ -0,0 +1,10 @@
+﻿const botonComienzo = document.getElementById("boton-comienzo");
+const portada = document.getElementById("portada");
+const simulador = document.getElementById("simulador");
+
+function iniciarSimulador() {
+  portada.hidden = true;
+  simulador.hidden = false;
+}
+
+botonComienzo.addEventListener("click", iniciarSimulador);
